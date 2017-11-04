7e4e5fd59904e4bfc1cc62548a6bbf9942ef23e2
domaindrivendev <domaindrivendev@gmail.com>
2017-11-04 15:23:29 -0700

    wip

--- build.ps1
+++ build.ps1
@@ -1,6 +1,16 @@
-Import-Module .\tools\psake\psake.psm1
-Invoke-psake .\build-definition.ps1
+#Import-Module .\tools\psake\psake.psm1
+#Invoke-psake .\build-definition.ps1
 
-# Get Psake to Return Non-Zero Return Code on Build Failure (https://github.com/psake/psake/issues/58)
-exit !($psake.build_success)
+## Get Psake to Return Non-Zero Return Code on Build Failure (https://github.com/psake/psake/issues/58)
+#exit !($psake.build_success)
 
+echo "Install dependencies"
+dotnet restore
+
+echo "Install swagger-ui assets"
+Push-Location src/Swashbuckle.AspNetCore.SwaggerUI
+bower install
+Pop-Location
+
+echo "Build"
+dotnet build -v minimal
